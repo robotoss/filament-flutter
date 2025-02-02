941c4af963a69c3a48ae0fa69a8748633c69c608
Konstantin Yeftifeyev <zoxo@outlook.com>
2025-02-02 19:43:03 +0200

    feat: Add simle iOS render

--- filament_flutter_ios/ios/Classes/FilamentView.mm
+++ filament_flutter_ios/ios/Classes/FilamentView.mm
@@ -7,9 +7,25 @@
 
 #import "FilamentView.h"
 
+#include "FilamentView.h"
+
+#include <filament/Camera.h>
 #include <filament/Engine.h>
+#include <filament/IndexBuffer.h>
+#include <filament/Material.h>
+#include <filament/RenderableManager.h>
+#include <filament/Renderer.h>
+#include <filament/Scene.h>
+#include <filament/TransformManager.h>
+#include <filament/VertexBuffer.h>
+#include <filament/View.h>
+#include <filament/Viewport.h>
+
+#include <utils/EntityManager.h>
 
 using namespace filament;
+using utils::Entity;
+using utils::EntityManager;
 
 @implementation FilamentViewFactory {
     NSObject<FlutterBinaryMessenger>* _messenger;
@@ -39,9 +55,46 @@ using namespace filament;
 
 @end
 
+struct App {
+    VertexBuffer* vb;
+    IndexBuffer* ib;
+    Material* mat;
+    Entity renderable;
+};
+
+struct Vertex {
+    filament::math::float2 position;
+    uint32_t color;
+};
+
+static const Vertex TRIANGLE_VERTICES[3] = {
+    {{1, 0}, 0xffff0000u},
+    {{cos(M_PI * 2 / 3), sin(M_PI * 2 / 3)}, 0xff00ff00u},
+    {{cos(M_PI * 4 / 3), sin(M_PI * 4 / 3)}, 0xff0000ffu},
+};
+
+static constexpr uint16_t TRIANGLE_INDICES[3] = { 0, 1, 2 };
+
+// This file is compiled via the matc tool. See the "Run Script" build phase.
+static constexpr uint8_t BAKED_COLOR_PACKAGE[] = {
+#include "../Materials/bakedColor.mat"
+};
+
+
 @implementation FilamentView {
     UIView *_view;
-    Engine* _engine;
+    Engine* engine;
+    Renderer* renderer;
+    Scene* scene;
+    View* filaView;
+    Camera* camera;
+    SwapChain* swapChain;
+    App app;
+    CADisplayLink* displayLink;
+
+    // The amount of rotation to apply to the camera to offset the device's rotation (in radians)
+    float deviceRotation;
+    float desiredRotation;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -50,7 +103,55 @@ using namespace filament;
               binaryMessenger:(NSObject<FlutterBinaryMessenger>*)messenger {
     if (self = [super init]) {
         _view = [[UIView alloc] init];
-        _engine = Engine::create(Engine::Backend::METAL);
+        engine = Engine::create(Engine::Backend::METAL);
+        
+        ///
+        swapChain = engine->createSwapChain((__bridge void*) self.layer);
+         renderer = engine->createRenderer();
+         scene = engine->createScene();
+         Entity c = EntityManager::get().create();
+         camera = engine->createCamera(c);
+         renderer->setClearOptions({.clearColor={0.1, 0.125, 0.25, 1.0}, .clear = true});
+
+         filaView = engine->createView();
+         filaView->setPostProcessingEnabled(false);
+
+         app.vb = VertexBuffer::Builder()
+             .vertexCount(3)
+             .bufferCount(1)
+             .attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
+             .attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
+             .normalized(VertexAttribute::COLOR)
+             .build(*engine);
+         app.vb->setBufferAt(*engine, 0,
+                             VertexBuffer::BufferDescriptor(TRIANGLE_VERTICES, 36, nullptr));
+
+         app.ib = IndexBuffer::Builder()
+             .indexCount(3)
+             .bufferType(IndexBuffer::IndexType::USHORT)
+             .build(*engine);
+         app.ib->setBuffer(*engine,
+                           IndexBuffer::BufferDescriptor(TRIANGLE_INDICES, 6, nullptr));
+
+         app.mat = Material::Builder()
+             .package((void*) BAKED_COLOR_PACKAGE, sizeof(BAKED_COLOR_PACKAGE))
+             .build(*engine);
+
+         app.renderable = EntityManager::get().create();
+         RenderableManager::Builder(1)
+             .boundingBox({{ -1, -1, -1 }, { 1, 1, 1 }})
+             .material(0, app.mat->getDefaultInstance())
+             .geometry(0, RenderableManager::PrimitiveType::TRIANGLES, app.vb, app.ib, 0, 3)
+             .culling(false)
+             .receiveShadows(false)
+             .castShadows(false)
+             .build(*engine, app.renderable);
+         scene->addEntity(app.renderable);
+
+         filaView->setScene(scene);
+         filaView->setCamera(camera);
+         CGRect nativeBounds = [UIScreen mainScreen].nativeBounds;
+         filaView->setViewport(Viewport(0, 0, nativeBounds.size.width, nativeBounds.size.height));
     }
     return self;
 }
@@ -58,5 +159,6 @@ using namespace filament;
 - (UIView*)view {
     return _view;
 }
+
 
 @end
--- filament_flutter_ios/ios/filament_flutter_ios.podspec
+++ filament_flutter_ios/ios/filament_flutter_ios.podspec
@@ -1,7 +1,3 @@
-#
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
-# Run `pod lib lint filament_flutter_ios.podspec` to validate before publishing.
-#
 Pod::Spec.new do |s|
   s.name             = 'filament_flutter_ios'
   s.version          = '0.0.1'
@@ -13,25 +9,30 @@ Flutter widgets for Filament, at iOS.
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Konstantin' => 'zoxo@outlook.com' }
   s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.dependency 'Flutter'
-  s.platform = :ios, '12.0'
-  s.dependency 'Filament', '>= 1.51.1', '< 1.51.10'
+  s.source_files     = 'Classes/**/*', 'Materials/*'
+  s.dependency       'Flutter'
+  s.platform         = :ios, '12.0'
+  s.dependency       'Filament', '>= 1.51.1', '< 1.51.10'
   s.static_framework = true
+
   s.user_target_xcconfig = { 
     'DEFINES_MODULE' => 'YES', 
     'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386', 
-    "CLANG_CXX_LANGUAGE_STANDARD" => "c++17",
+    'CLANG_CXX_LANGUAGE_STANDARD' => 'c++17',
     'OTHER_CFLAGS' => '"-fvisibility=default" "$(inherited)"',
+    'USER_HEADER_SEARCH_PATHS' => '"${PODS_ROOT}/../.symlinks/plugins/filament_flutter_ios/ios/include" "${PODS_ROOT}/../.symlinks/plugins/filament_flutter_ios/ios/Materials" "$(inherited)"',
+    'ALWAYS_SEARCH_USER_PATHS' => 'YES'
   }
 
-  # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 
     'DEFINES_MODULE' => 'YES', 
     'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386',
-    "CLANG_CXX_LANGUAGE_STANDARD" => "c++17",
+    'CLANG_CXX_LANGUAGE_STANDARD' => 'c++17',
     'OTHER_CXXFLAGS' => '"--std=c++17" "-fmodules" "-fcxx-modules" "-fvisibility=default" "$(inherited)"',
     'OTHER_CFLAGS' => '"-fvisibility=default" "$(inherited)"',
+    'USER_HEADER_SEARCH_PATHS' => '"${PODS_ROOT}/../.symlinks/plugins/filament_flutter_ios/ios/include" "${PODS_ROOT}/../.symlinks/plugins/filament_flutter_ios/ios/Materials" "$(inherited)"',
+    'ALWAYS_SEARCH_USER_PATHS' => 'YES'
   }
+
   s.swift_version = '5.0'
 end
